d3065738d27ce1e00ca20f9d0870129ceb70b629
Cyne-wulf <acynewulf@gmail.com>
2024-10-22 19:39:06 -0700

    Initial push for lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,20 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - This is the logic file for the lab6 assignment
+// Author: Ashan Devine
+// Date: 10/22/24
 
-// Constants
+// Declarations:
+
+let myTransport=["EBike", "Car", "Rides from my roommate", "Rides from my friends", "My Legs"];
+
+let myMainRide = {
+  make: "Honda",
+  model:"Accord",
+  color: "Silver",
+  year: "2017",
+  age: function() {
+      return 2024 - this.year;
+      }
+  };
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  document.writeln("For getting around, I use: " + myTransport + "<br>");
+  document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 }
 
 // let's get this party started
