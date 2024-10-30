be04101ce28307582cbcf2c3265d0ab685d118af
Cyne-wulf <acynewulf@gmail.com>
2024-10-30 14:37:42 -0700

    Lab8 Formatted

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -3,13 +3,12 @@
 // Date: 10/29/24
 
 // Constants
-array = [0,1,2]
+array = [0, 1, 2];
 
 // Functions
-function timesTwo(x){
-     let results = x * 2;
-    return results;
-
+function timesTwo(x) {
+  let results = x * 2;
+  return results;
 }
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
@@ -21,12 +20,11 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
-  array.map(timesTwo)
-  let mapResults = array.map(timesTwo)
-  console.log("Results with map function: ", mapResults)
+  array.map(timesTwo);
+  let mapResults = array.map(timesTwo);
+  console.log("Results with map function: ", mapResults);
 
-
-  let squareResults = array.map(function(x) {
+  let squareResults = array.map(function (x) {
     var results = x * x;
     return results;
   });
@@ -34,10 +32,15 @@ function main() {
 
   // $("#output").html(mapResults);
   // $("#output").html(squareResults);
-  
+
   $("#output").html(
-    "<h3>Script Output</h3> <p>Results with the function 'timesTwo': " + mapResults.join(" ") + ". Results with anonymous function 'square': " + squareResults.join(" ") + "</p>");
-  }
+    "<h3>Script Output</h3> <p>Results with the function 'timesTwo': " +
+      mapResults.join(" ") +
+      ". Results with anonymous function 'square': " +
+      squareResults.join(" ") +
+      "</p>"
+  );
+}
 
 // let's get this party started
 main();
