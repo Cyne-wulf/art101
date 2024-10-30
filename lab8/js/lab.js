361e9e24aa0e353965b873ebdc16d07333f8957f
Cyne-wulf <acynewulf@gmail.com>
2024-10-30 14:31:21 -0700

    Lab8 Initial Commit

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,10 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - This is the logic file for lab 8.
+// Author: Ashan Devine
+// Date: 10/29/24
 
 // Constants
+array = [0,1,2]
 
 // Functions
+function timesTwo(x){
+     let results = x * 2;
+    return results;
+
+}
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -15,7 +21,23 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
-}
+  array.map(timesTwo)
+  let mapResults = array.map(timesTwo)
+  console.log("Results with map function: ", mapResults)
+
+
+  let squareResults = array.map(function(x) {
+    var results = x * x;
+    return results;
+  });
+  console.log("Results with anonymous function (square): ", squareResults);
+
+  // $("#output").html(mapResults);
+  // $("#output").html(squareResults);
+  
+  $("#output").html(
+    "<h3>Script Output</h3> <p>Results with the function 'timesTwo': " + mapResults.join(" ") + ". Results with anonymous function 'square': " + squareResults.join(" ") + "</p>");
+  }
 
 // let's get this party started
 main();
