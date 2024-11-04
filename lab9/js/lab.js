3e33d254fa055b5a8cad5cae34b4fbc255675100
Cyne-wulf <acynewulf@gmail.com>
2024-11-04 13:23:53 -0800

    Lab9 Initial Commit

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,21 +1,42 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+// index.js - This is the logic file for the lab9 assignment
+ * Author: Ashan Devine <atdevine@ucsc.edu>
+ * Date: 11/04/24
+ */
 
-// Constants
+// Using jQuery to add buttons to each section
+const defaultAudio = document.getElementById('default-audio');
+const partyAudio = document.getElementById('party-audio');
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+$("#party-toggle button").click(function() {  //Toggles the party mode
+    if ($("body").hasClass("party")) {
+        // Switch back to default mode
+        $("body").removeClass("party");
+        $("#party-toggle button").html("Party");  //Updates button to provide party option
+        partyAudio.pause(); //Pauses party audio
+        partyAudio.currentTime = 0; // Reset party audio
+        defaultAudio.play(); // Start default audio
+    } else {
+        // Switch to party mode
+        $("body").addClass("party");  //Adds class to body, party
+        $("#party-toggle button").html("Professional"); //Updates the button to provide professional option
+        defaultAudio.pause(); //Pauses party audio
+        defaultAudio.currentTime = 0; // Reset default audio
+        partyAudio.play(); // Start party audio
+    }
+});
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+$(".sectionbox").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>");  //Adds buttons to sections
+
+
+$(document).on("click", ".highlight-button", function() { //Add toggle class functionality to button
+    $(this).parent().toggleClass("special");  // Toggles the "special" class on the parent section of the clicked button
+});
+
+
+// Click event handler for the "make-hide" button
+$("#make-hide").click(function() {
+    $("#instructions").toggleClass("hidden"); // Toggles the "hidden" class on the element with ID "instructions"
+});
